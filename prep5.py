f2555f06b5e2ffe83da88aba6fa1507490afa545
LieriheartHuang <89817234+LieriheartHuang@users.noreply.github.com>
2021-09-03 12:07:40 -0700

    Update and rename prep5 to prep5.py

--- prep5.py
+++ prep5.py
@@ -1,6 +1,6 @@
 b=input("Enter food:\n")
 a=input("Enter price:\n")
-if a<20
+if a<=20
 print("That "+b+" is affordable.")
 if a>20
-print（“That ”+b+“ is expensive.”）
+print("That "+b+" is expensive.”）
